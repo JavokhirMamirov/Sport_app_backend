b84605068acbe70d18d7d00c6c03e526ec1d5b32
Mamirov1997 <mamirovjavoxir@gmail.com>
2021-12-15 12:06:15 +0500

    Connect template

---
+++ sport/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from sport.views import HomeView, ObjectsView, AddNewObjectView, ObjectDetailView
+
+urlpatterns = [
+    path('', HomeView, name="home"),
+    path('sport-object', ObjectsView, name="sport-object"),
+    path('add-sport-object', AddNewObjectView, name="add-sport-object"),
+    path('sport-object-detail', ObjectDetailView, name="sport-object-detail"),
+]
--- sport/views.py
+++ sport/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def HomeView(request):
+    return render(request, 'index.html')
+
+def ObjectsView(request):
+    return render(request, 'objects.html')
+
+def AddNewObjectView(request):
+    return render(request, 'add_new_object.html')
+
+def ObjectDetailView(request):
+    return render(request, 'object_detail.html')
