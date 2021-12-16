016caa3ba9afb090fd5c086e29dfb449c69c5b3f
Mamirov1997 <mamirovjavoxir@gmail.com>
2021-12-16 12:00:26 +0500

    map

---
+++ static/assets/map.js
@@ -0,0 +1,22 @@
+function initMap() {
+    let Lat = 40.375056
+    let Lng = 69.1585715
+    let mapOptions = {
+        center: {lat: Lat, lng: Lng},
+        zoom: 6
+    }
+
+    let map = new google.maps.Map(document.getElementById('simple-map'), mapOptions)
+
+    let markerOptions = {
+        position: new google.maps.LatLng(Lat, Lng)
+    }
+
+    let marker = new google.maps.Marker(markerOptions)
+
+    marker.setMap(map)
+    map.addListener("click", (clickEvent) => {
+        marker.setPosition(clickEvent.latLng)
+        document.getElementById('location_inp').value = clickEvent.latLng.lat() + "," + clickEvent.latLng.lng()
+    });
+}
