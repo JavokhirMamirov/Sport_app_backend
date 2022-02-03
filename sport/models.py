3fc9c4a20f4a8584c693ecba1055aa324b8ae25f
Boburjon Dadajonov <dadajonovboburjon3@gmail.com>
2022-02-03 15:28:52 +0500

    dashboard for users 1

--- sport/models.py
+++ sport/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 from location_field.models.plain import PlainLocationField
@@ -44,6 +45,7 @@ class SportObject(models.Model):
     type = models.ForeignKey(ObjectType, on_delete=models.CASCADE, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     is_active = models.BooleanField(default=True)
+    user = models.ManyToManyField(User, related_name='objects')
 
     def __str__(self):
         return self.name
--- sport/urls.py
+++ sport/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path,include
 
-from sport.views import HomeView, ObjectsView, AddNewObjectView, ObjectDetailView, UpdateObjectView, login_view, \
-    AddTypeUsers, \
-    AddObjectCategory, CreateObject, DeleteObjectView, UpdateObject, add_image, delete_image, notf, server,user_logout, \
-    order_new, order_accepted, order_not_accepted, delete_order, change_order
+from sport.views import *
 
 urlpatterns = [
     path('', HomeView, name="home"),
@@ -13,6 +10,7 @@ urlpatterns = [
     path('add-sport-object/', AddNewObjectView, name="add-sport-object"),
     path('sport-object-detail/<int:pk>/', ObjectDetailView, name="sport-object-detail"),
     path('login/', login_view, name="login"),
+    path('sign-up', user_create),
     path('logout/', user_logout, name="logout"),
     ## action
     path('add-type-users', AddTypeUsers, name='add-type-url'),
